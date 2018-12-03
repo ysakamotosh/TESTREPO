5889181503e381b23dc8cc5dd228ec73e9b9233a
catadmin <catadmin@localhost.localdomain>
2018-12-03 17:08:46 +0900

    INTFUN-1 : 実行管理画面の消化数と予定入力画面の消化数が異なる

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 class Hello {
     public static void main(String args []) {
 	System.out.println("Hello World.");
-	System.out.println("Hello CAT.");
 	System.out.println("Hello SHIFT.");
+	System.out.println("Hello CATCAT.");
     }
 }
