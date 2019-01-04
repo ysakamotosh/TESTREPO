00c163f604893f100f9e4c8216c4489bdf9d3853
catadmin <catadmin@localhost.localdomain>
2019-01-04 15:38:48 +0900

    INTFUN-1 : 実行管理画面の消化数と予定入力画面の消化数が異なる

--- sample.java
+++ sample.java
@@ -2,26 +2,6 @@ class Hello {
     public static void main(String args []) {
 	System.out.println("Hello World.");
 	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
-	System.out.println("Hello World.");
 	System.out.println("Hello SHIFT.");
 	System.out.println("Hello CATCAT.");
 	System.out.println("Hello CATCAT.");
