6c215fcd1bbcc504ce299321f558e79614164302
yuta <yuta.sakamoto@shiftinc.jp>
2019-06-27 14:15:31 +0900

    DEMOL-1

--- sample.js
+++ sample.js
@@ -1,3 +1,5 @@
 console.log("Modern test management platform for development");
 console.log("Modern test management platform for development");
 console.log("Modern test management platform for development");
+console.log("Hello CAT");
+console.log("Test Managent tool");
